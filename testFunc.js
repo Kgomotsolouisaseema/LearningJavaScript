e000f7ccb5012814ac4bd5a62615ef1eb5bdc032
Kgomotso <kgomotsolouisaseema@gmail.com>
2023-06-19 15:57:38 +0200

    Functions as

---
+++ testFunc.js
@@ -0,0 +1,14 @@
+function greet (name , lastname){
+    console.log('hi', " " + name ,lastname);
+}
+greet( "Kgomotso" ,"Seema" );
+greet("Lethabo" ,  "Motou")
+
+
+//String Primitive
+const message = 'this is my first msg';
+
+
+//string object
+const another = new String('hi');
+console.log(another)
